fef0942628a8003ec8e6c1565e85e25bf621159d
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-01 11:49:50 -0600

    add more monitor types to monitor recipe for testing

--- test/cookbooks/splunk_forwarder_test/recipes/monitor.rb
+++ test/cookbooks/splunk_forwarder_test/recipes/monitor.rb
@@ -2,8 +2,37 @@
 include_recipe 'splunk_forwarder_test::default'
 
 # Option hash for monitor options
-option_hash = { recursive: true }
-# Define a splunk_monitor_instance
+file_option_hash = { recursive: true }
+# Define a file/directory splunk_monitor instance
 splunk_monitor '/var/log' do
-  monitor_options option_hash
+  monitor_options file_option_hash
+end
+
+# Define a TCP splunk_monitor instance
+tcp_option_hash = { listenOnIPv6: 'no', queueSize: '1MB' }
+
+splunk_monitor ':5514' do
+  monitor_type 'tcp'
+  monitor_options tcp_option_hash
+end
+
+# Define a TCP splunk_monitor instance
+splunktcp_option_hash = { sourcetype: 'syslog' }
+
+splunk_monitor '10.2.0.1:9996' do
+  monitor_type 'splunktcp'
+  monitor_options splunktcp_option_hash
+end
+
+# Define a WinEventLog splunk_monitor instance
+# This just tests that the stanza is built properly
+# Only use this monitor type on Windows systems
+winevent_option_hash = { start_from: 'newest',
+                         disabled: 0,
+                         index: 'winevents',
+                         blacklist1: '4656,4658' }
+
+splunk_monitor 'Security' do
+  monitor_type 'winevent'
+  monitor_options winevent_option_hash
 end
