da091e9eec350085c823bf29db37a784b916b018
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-01 09:57:33 -0600

    expand test cookbook monitor recipe to test monitor options functionality

--- test/cookbooks/splunk_forwarder_test/recipes/monitor.rb
+++ test/cookbooks/splunk_forwarder_test/recipes/monitor.rb
@@ -1,5 +1,9 @@
 # Pull in default recipe so we have the forwarder installed
 include_recipe 'splunk_forwarder_test::default'
 
+# Option hash for monitor options
+option_hash = { recursive: true }
 # Define a splunk_monitor_instance
-splunk_monitor '/var/log'
+splunk_monitor '/var/log' do
+  monitor_options option_hash
+end
