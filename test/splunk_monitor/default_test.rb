bf2f17121d88211163d12f5c8523ce826b8f1514
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-01 08:13:32 -0600

    update monitor tests to expect new dir naming scheme

--- test/splunk_monitor/default_test.rb
+++ test/splunk_monitor/default_test.rb
@@ -1,9 +1,9 @@
-describe file('/opt/splunkforwarder/etc/apps/00_autogen_inputs/') do
+describe file('/opt/splunkforwarder/etc/apps/00_autogen_inputs__var_log/') do
   it { should exist }
   it { should be_owned_by 'test_user' }
 end
 
-describe file('/opt/splunkforwarder/etc/apps/00_autogen_inputs/local/inputs.conf') do
+describe file('/opt/splunkforwarder/etc/apps/00_autogen_inputs__var_log/local/inputs.conf') do
   it { should exist }
   it { should be_owned_by 'test_user' }
 end
