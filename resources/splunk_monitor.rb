5562c4f65e09d9c24a379af04070ace453a0ea6a
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-01 07:01:17 -0600

    add a skeleton monitor resource and test file for it

---
+++ resources/splunk_monitor.rb
@@ -0,0 +1,13 @@
+#[monitor:///var/log]
+#[tcp://:9997]
+#[splunktcp://10.1.1.100:9996]
+#[splunktcp-ssl:9996]
+#[fschange:/etc/]
+#[WinEventLog://Security]
+# [perfmon://LocalPhysicalDisk]
+# [admon://DefaultTargetDC]
+
+resource_name :splunk_monitor
+
+property :path, String, name_property: true
+property :monitor_type, String, default: "file" # %w(file fschange winevent tcp splunktcp admon perfmon)
