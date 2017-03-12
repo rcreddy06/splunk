6e8ee7342c7645d747159e51acf53b9eabf12d4b
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-12 16:36:23 -0500

    build out install action for splunk_server resource. a lot of this is identical to the UF install action. gotta be a better way to abstract this out and dry things up...

--- resources/splunk_server.rb
+++ resources/splunk_server.rb
@@ -33,3 +33,69 @@ action_class do
     end
   end
 end
+
+action :install do
+  # for those odd RHEL systems
+  package 'tar'
+
+  group new_resource.splunk_group do
+    action :create
+    append true
+  end
+
+  user new_resource.splunk_user do
+    gid new_resource.splunk_group
+    shell '/bin/false'
+    system true
+    action :create
+  end
+
+  directory 'splunk install dir' do
+    mode '0755'
+    path new_resource.install_path
+    recursive true
+    owner new_resource.splunk_user
+    group new_resource.splunk_group
+  end
+
+  remote_file "splunk checksum" do
+    source checksum_uri
+    path "#{Chef::Config['file_cache_path']}/#{new_resource.tarball_name}.md5"
+  end
+
+  remote_file "splunk tarball" do
+    source tarball_uri
+    path "#{Chef::Config['file_cache_path']}/#{new_resource.tarball_name}"
+    verify { |file| validate_checksum(file) }
+  end
+
+  execute 'extract splunk tarball' do
+    command extraction_command
+    action :run
+    creates ::File.join(new_resource.install_path, 'LICENSE')
+  end
+
+  # make sure the instance's user owns the instance install dir
+  execute "chown install dir as #{new_resource.splunk_user}" do
+    command "chown -R #{new_resource.splunk_user}:#{new_resource.splunk_group} #{new_resource.install_path}"
+    action :run
+    not_if { Etc.getpwuid(::File.stat("#{new_resource.install_path}/license-eula.txt").uid) == new_resource.splunk_user }
+  end
+
+  # enable start a boot
+  execute "#{new_resource.install_path}/bin/splunk enable boot-start -user #{new_resource.splunk_user} --accept-license" do
+    only_if { ::File.exist? "#{new_resource.install_path}/ftr" }
+  end
+
+  # create splunk service
+  service 'splunk' do
+    supports status: true, restart: true
+    action [:enable, :start]
+    case node['init_package']
+    when 'systemd'
+      provider Chef::Provider::Service::Systemd
+    else
+      provider Chef::Provider::Service::Init
+    end
+  end
+end
